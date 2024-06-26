e7bdcefcc298634b3c61c7c7855520e95cc2a2aa
DivyaBijili9 <divya.bijili@ibm.com>
2024-06-26 18:58:48 +0530

    Add files via upload

---
+++ Cucumber/Activities/StepDefinition/BaseClass.java
@@ -0,0 +1,21 @@
+package stepDefinition;
+
+
+import io.cucumber.java.BeforeAll;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseClass {
+
+    static WebDriver driver;
+    static WebDriverWait wait;
+
+
+
+
+}
